aa5fa1f10a297a77da7a3b0db0aff057f7884627
katafuchix <katafuchix@gmail.com>
2025-03-20 11:22:13 +0900

    Initial Commit

---
+++ ChipSelection/ChipSelectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChipSelectionApp.swift
+//  ChipSelection
+//
+//  Created by cano on 2025/03/20.
+//
+
+import SwiftUI
+
+@main
+struct ChipSelectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
